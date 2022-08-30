74d5a0502822fe4504d7cd6949e3c26d6295df1c
Volodymyr Ok <v.shark.t@gmail.com>
2022-08-30 16:27:57 +0300

    Усі зображення + меню + модалка

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,5 @@
 (() => {
-  const menuBtnRef = document.querySelector('[data-menu-button]');
+  const menuBtnRef = document.querySelector('.data-menu-button');
   const mobileMenuRef = document.querySelector('[data-menu]');
   const mobileBtnClose = document.querySelector('.data-menu-close');
 
